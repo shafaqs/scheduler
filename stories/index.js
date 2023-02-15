496bebb3d8b2923113ff4b6f09a91d11ce7ad7ae
Shafaq Saud <shafaq.saud@gmail.com>
2023-02-15 14:44:30 -0500

    Create schedule

--- stories/index.js
+++ stories/index.js
@@ -200,7 +200,7 @@ storiesOf("Appointment", module)
         time="4pm"
         interview={{ student: "Lydia Miller-Jones", interviewer }}
       />
-      <Appointment time="5pm" />
+      <Appointment key="last" time="5pm" />
     </Fragment>
   ));
 
