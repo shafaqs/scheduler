be788529faaad407ad2bba8cd214a924a57a4212
Shafaq Saud <shafaq.saud@gmail.com>
2023-02-17 19:34:58 -0500

    Update interviewer

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
-import { getAppointmentsForDay } from '../helpers/selectors';
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
 
 
@@ -57,23 +57,25 @@ export default function Application(props) {
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
       const [days, appointments] = all;
-      setState(prev => ({ ...prev, days: days.data, appointments: appointments.data }));
+      setState(prev => ({ ...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data }));
     });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
     return (
       <Appointment
-        key={appointment?.id}
-        id={appointment?.id}
-        time={appointment?.time}
-        interview={appointment?.interview}
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
