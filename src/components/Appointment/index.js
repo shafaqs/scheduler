ac805fdb088731dae8a0fd61af8108ea977424cd
Shafaq Saud <shafaq.saud@gmail.com>
2023-02-19 18:28:53 -0500

    Update Index.js &form.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,18 +5,36 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 
 export default function Appointment(props) {
-  return <article className="appointment">
-    <Header time={props.time} />
-    {props.interview ? (
-      <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-      />
-    ) : (
-      <Empty />
-    )}
-  </article>;
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE &&
+        <Form
+          interviewers={[]}
+          onCancel={back}
+          onSave={(student, interviewer) => console.log(student, interviewer)}
+        />}
+    </article>
+  );
 }
